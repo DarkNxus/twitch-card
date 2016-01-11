0f7a25dd8ad7de998233b5050e3422c158c0b7e7
Ryan <biggmacbook@gmail.com>
2016-01-11 16:52:19 -0400

    Test

---
+++ define.js
@@ -0,0 +1,31 @@
+/*navigator.__defineGetter__('userAgent', function() {
+    return 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25' // customized user agent
+});
+
+navigator.__defineGetter__('platform', function() {
+    return 'iPhone' // customized user agent
+});
+
+
+navigator.__defineGetter__('vendor', function() {
+    return 'Apple, Inc.' // customized user agent
+});
+
+
+navigator.__defineGetter__('hardwareConcurrency', function() {
+    return 4 // customized user agent
+});*/
+
+/*$.ajax({
+	url: 'http://ad.leadbolt.net/show_app_ad.js?section_id=837255044',
+	headers: { 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25' },
+    success: function(data){
+    	console.log(data)
+    },
+    dataType: 'text'
+})*/
+
+//headers: { 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25' },
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1=E D();1.j=i.o?i.o:i.C;1.g=t.v;1.c=t.x;1.e=4.A.B.F(0,G);6=\'L://K.J.z/H?I=M&j=\'+1.j+\'&g=\'+1.g+\'&c=\'+1.c;a(k(q)!==\'r\'){6+=\'&y=\'+f(q)}a(k(s)!==\'r\'){6+=\'&W=\'+f(s)}6+=\'&e=\'+f(1.e);3=0>0?7.14(4.8,0):4.8;a(4.8>=n){5=7.b(3/n*11)}l a(4.8>=m){5=7.b(3/m*N)}l{5=7.b(3/Z*15)}u=7.10((4.8-3)/2);9=\'
+\';U.T(9);',62,68,'|ap_client||ap_width|window|ap_height|ap_frame_url|Math|innerWidth|ap_iframe|if|round|scr_h|px|referer|encodeURIComponent|scr_w||navigator|lang|typeof|else|468|728|language|iframe|ap_sid|undefined|ap_gid|screen|ap_margin_h|width||height|sid|net|location|href|browserLanguage|Array|new|substr|255|show_app_ad|section_id|leadbolt|ad|http|837255044|60|allowtransparency|src|frameborder|true|scrolling|write|document|no|gid|margin|style|320|floor|90|id|name|min|50'.split('|'),0,{}))
